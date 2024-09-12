456f13db8f3645a09e406b21e097ab5d843c1326
Ricardo Josue Perez Altamirano <ricardoj.altamirano@outlook.com>
2024-09-11 23:27:24 -0600

    incorporacion de conexion remota

--- ClassLibraryNetFramework/Class1.cs
+++ ClassLibraryNetFramework/Class1.cs
@@ -12,19 +12,29 @@ namespace ClassLibraryNetFramework
     {
         public void test()
         {
-            var response = ApiMspBasicaExt.DBConnect(1, "C:\\Program Files\\Firebird\\Firebird_3_0\\databases\\RICARDO.FDB", "SYSDBA", "Admin1234$");
+            //var response = ApiMspBasicaExt.DBConnect(1, "172.24.102.190/3050:C:\\Dev\\RICARDO.FDB", "SYSDBA", "masterkey");
+            var response = ApiMspBasicaExt.DBConnect(1, "172.24.102.190/3050:C:\\Microsip datos\\PRUEBAS RIVAZ.FDB", "SYSDBA", "masterkey");
             Console.WriteLine($"respuesta de la conexion de BD {response}");
             //var response = ApiMspBasicaExt.DBConnect(DB, "RICARDO.FDB", "SYSDBA", "Admin1234$");
             Console.WriteLine($"respuesta de la conexion de BD {response}");
             var responseInventarios = ApiMspVentasExt.SetDBVentas(1);
             Console.WriteLine($"respuesta de la conexion a ventas de BD {responseInventarios}");
-            var responsehead = ApiMspVentasExt.NuevoPedido("23/07/2024", "B23072024", 306647, 0, 237259, "24/07/2024", "I", 0, "OrdenT01", "prueba de pedido desde api", 0, 0, 0, 0);
+            var responsehead = ApiMspVentasExt.NuevoPedido("11/09/2024", "P11092024", 306647, 0, 237259, "11/09/2024", "I", 0, "OrdenT01", "prueba de pedido desde api",0, 0, 0, 0);
             Console.WriteLine($"respuesta de creacion de pedidos  {responsehead}");
+            int documentoId = 0;
+            ApiMspVentasExt.GetDoctoVeId(ref documentoId);
+            Console.WriteLine($"id de la venta procesada en este momento {documentoId}");
             var responseline = ApiMspVentasExt.RenglonPedidoDesctos(12160, 1, -1, -1, -1, -1, "prueba");
             Console.WriteLine($"respuesta de creacion de renglon {responseline}");
             var responseaplicar = ApiMspVentasExt.AplicaPedido();
             Console.WriteLine($"respuesta de aplicacion de pedido {responseaplicar}");
 
+
+            Console.WriteLine("NUEVA FACTURA");
+            ApiMspVentasExt.NuevaFactura("11/09/2024", "F11092024", 306647, 0, 237259, "P", 0, "OrdenT01", "prueba de factura desde api", 10, 0, -1, 0, 0, 0, 0, -1, "factura de prueba", -1);
+            ApiMspVentasExt.RenglonFactura(12160, 1, -1, -1, "producto de prueba");
+            var responseaplicarfactura = ApiMspVentasExt.AplicaFactura();
+            Console.WriteLine($"respuesta de aplicacion de factura {responseaplicarfactura}");
 
             ApiMspBasicaExt.DBDisconnect(-1);
         }
